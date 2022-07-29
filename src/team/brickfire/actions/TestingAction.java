86745b910df66ad1ec6c4bb5319e0cf6c407d61e
Alexander Vogt <alexander_vogt@outlook.de>
2022-07-29 18:54:12 +0200

    Some JavaDoc

--- src/team/brickfire/actions/TestingAction.java
+++ src/team/brickfire/actions/TestingAction.java
@@ -4,12 +4,24 @@ import team.brickfire.actions.circuit_drive.CircuitDrive;
 import team.brickfire.actions.circuit_drive.CircuitOrientation;
 import team.brickfire.actions.circuit_drive.CircuitPosition;
 
+/**
+ * <p>Action is used for testing</p>
+ *
+ * @version 1.0
+ * @author Team BrickFire
+ */
 public class TestingAction extends BaseAction {
 
+    /**
+     * <p>Creates the testing action</p>
+     */
     public TestingAction() {
         super();
     }
 
+    /**
+     * <p>Gets executed</p>
+     */
     public void test() {
         CircuitDrive circuit = new CircuitDrive(CircuitPosition.SOUTH_EAST, CircuitOrientation.WEST);
 
